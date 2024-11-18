6132a31dd1577cf49c35c0b5fb94848bae8af698
ngxx-fus <msnp@outlook.com.vn>
2024-11-18 09:20:12 +0700

    U: lab3wfirebase

--- lab3_itfa_firebase_test/src/main.cpp
+++ lab3_itfa_firebase_test/src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include "DHT.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define FIREBASE_HOST "https://lab3-itfa-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "SftGzo6EJMJqpTa8i8DphVnYxwfdugy23FuNlcRE"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define DHTTYPE DHT11
+#define DHT_DAT_PIN 21
+DHT dht(DHT_DAT_PIN, DHTTYPE);
+
+#define LED_CTL_PIN_1 21
+#define INDICATOR_LED_PIN 23
+#define LED_CTL_PIN_2 19
+
+void Sync_Sensor_Data()
+{
+  digitalWrite(INDICATOR_LED_PIN, HIGH);
+  delay(4000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if( isnan(t) || isnan(h)){
+        Serial.println("DHT11 - E: Corrupted data!");
+        digitalWrite(INDICATOR_LED_PIN, LOW);
+        delay(100);
+        digitalWrite(INDICATOR_LED_PIN, HIGH);
+        delay(100);
+        digitalWrite(INDICATOR_LED_PIN, LOW);
+        delay(100);
+        digitalWrite(INDICATOR_LED_PIN, HIGH);
+      }else{
+      Serial.println("Temp: ");
+      Serial.println(t);
+      Serial.println("Humid: ");
+      Serial.println(h);
+  }
+  // Tắt đèn INDICATOR để thông báo khi quá trình đọc dừng lại
+  digitalWrite(INDICATOR_LED_PIN, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup(){
+  Serial.begin(9600);
+  Serial.println(("\n\nHello!\n From MEOW GROUP!"));
+  pinMode(LED_CTL_PIN_1, OUTPUT);
+  pinMode(INDICATOR_LED_PIN, OUTPUT);
+  pinMode(LED_CTL_PIN_2, OUTPUT);
+  dht.begin();
+  //
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop(){
+  Sync_Sensor_Data();
+  delay(1000);
 }
